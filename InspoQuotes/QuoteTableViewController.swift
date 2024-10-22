16e61bb6011a798baf11a140688ee72fa3b31634
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-10-22 21:53:58 +0900

    identifier の修正

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -37,15 +37,15 @@ class QuoteTableViewController: UITableViewController {
         return quotesToShow.count
     }
     
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        
+        
+        
+        return cell
+    }
+    
     
     /*
      // Override to support conditional editing of the table view.
